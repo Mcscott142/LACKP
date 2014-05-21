edff15f200f2176f0bdcc4219f0fa9c7c3294801
Colin Scott <m.colin.scott@gmail.com>
2014-05-21 13:35:15 -0400

    Final

--- server.rb
+++ server.rb
@@ -1,18 +1,37 @@
-# require 'sinatra'
+require 'sinatra'
 require 'csv'
-teams = {}
-CSV.foreach('lackp_starting_rosters.csv', headers: true) do |row|
-  # player_info << row.to_hash
-  first_name = row["first_name"]
-  last_name = row["last_name"]
-  position = row["position"]
-  team = row["team"]
+require 'pry'
+require 'uri'
 
-  if !teams.has_key?(team)
-    teams[team] = []
+def read_in_teams
+  @teams = {}
+  CSV.foreach('lackp_starting_rosters.csv', headers: true) do |row|
+    # player_info << row.to_hash
+    first_name = row["first_name"]
+    last_name = row["last_name"]
+    position = row["position"]
+    team = row["team"]
+
+    if !@teams.has_key?(team)
+      @teams[team] = []
+    end
+
+    @teams[team] << [first_name, last_name, position]
   end
-
-  teams[team] << [first_name, last_name, position]
+  return @teams
 end
 
-puts teams.inspect
+# puts read_in_teams.keys
+
+get '/' do
+  @data = read_in_teams()
+  erb :index
+end
+
+get "/teams/:team_name" do
+  @team_name = params[:team_name]
+  @data = read_in_teams
+  #binding.pry
+  erb :teampage
+end
+
