13571f1e1896637106a78078f1d01a93aa086f5c
Colin Scott <m.colin.scott@gmail.com>
2014-05-23 14:25:31 -0400

    Final kickball without CSS styling

--- server.rb
+++ server.rb
@@ -1,7 +1,7 @@
 require 'sinatra'
 require 'csv'
 require 'pry'
-#require 'sinatra/reloader'
+require 'sinatra/reloader'
 
 def import_file(csv)
    players = []
@@ -21,6 +21,16 @@ def sort_teams(team)
   roster
 end
 
+def sort_positions(position)
+  players = import_file('lackp_starting_rosters.csv')
+  position_breakdown = []
+
+  players.each do |player|
+    position_breakdown << player if player[:position] == position
+  end
+  position_breakdown
+end
+
 
 get '/teams' do
   @players = import_file('lackp_starting_rosters.csv')
@@ -29,9 +39,15 @@ get '/teams' do
     if !@team_names.include?(player[:team])
       @team_names << player[:team]
     end
-    #binding.pry
-  end
 
+    @positions = []
+    @players.each do |player|
+      if !@positions.include?(player[:position])
+        @positions << player[:position]
+      end
+    end
+
+  end
   erb :index
 
 end
@@ -40,5 +56,10 @@ get '/teams/:team' do
   @team = sort_teams(params[:team])
   #binding.pry
   erb :show
+end
+
+get '/positions/:position' do
+  @positions = sort_positions(params[:position])
+  erb :position_page
 end
 
